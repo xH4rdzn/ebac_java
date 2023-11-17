75a69774384ed038810233d9c91a17d027c0b0be
Igor Coelho <xh4rdzn@gmail.com>
2023-11-17 09:14:02 -0300

    Adicionando Controle de fluxos

--- Media.java
+++ Media.java
@@ -2,19 +2,34 @@ import java.util.Scanner;
 
 public class Media {
     public static void main(String args[]) {
+        verificaAluno(calculaMedia());
+    }
+
+    public static int calculaMedia(){
         int nota1, nota2, nota3, nota4, media;
 
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite a primeira nota do aluno(a): ");
+        System.out.print("Digite a primeira nota do aluno(a): ");
         nota1 = entrada.nextInt();
-        System.out.println("Digite a segunda nota do aluno(a): ");
+        System.out.print("Digite a segunda nota do aluno(a): ");
         nota2 = entrada.nextInt();
-        System.out.println("Digite a terceira nota do aluno(a): ");
+        System.out.print("Digite a terceira nota do aluno(a): ");
         nota3 = entrada.nextInt();
-        System.out.println("Digite a quarta nota do aluno(a): ");
+        System.out.print("Digite a quarta nota do aluno(a): ");
         nota4 = entrada.nextInt();
 
         media = (nota1 + nota2 + nota3 + nota4) / 4;
-        System.out.println(media);
+        return  media;
+
+    }
+
+    public static void verificaAluno(int media) {
+        if (media >= 7) {
+            System.out.println("O Aluno está Aprovado !");
+        } else if (media >= 5 && media < 7) {
+            System.out.println("O Aluno está de Recuperação !");
+        } else {
+            System.out.println("O Aluno está Reprovado !");
+        }
     }
 }
